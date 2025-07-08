cde2763e58b6f8d838f08c4be1dab4048d8e079a
Naveenkumar D <146437848+Naveenkumar3327@users.noreply.github.com>
2025-07-08 09:24:56 +0530

    Add files via upload

---
+++ Day-2/script.js
@@ -0,0 +1,72 @@
+// // // console.log("Hello World from script.js");
+// // // console.log("This is another script file from another folder.");
+
+// // // var a = 10;
+// // // var a = 20;
+// // // console.log(a);
+
+
+// // let product_key = "abcd_efgh_ijkl-1234"
+// // product_key = "earth is flat XD";
+
+// // const a; // this is not allowed
+// // a = 20;
+
+// // console.log("Intializing Product Key :", product_key);
+
+
+// /*
+
+// for(intialization; condition; upgraftion)
+//  {
+// //code 
+// }
+
+// */
+// let i = 1;
+// // for (let i = 1; i <= 10; i++) {
+// //     console.log(i);
+// // }
+// // do {
+// //     console.log(i)
+// // }
+// // while (i < 10) {
+// //     console.log(i);
+// // }
+
+
+
+// let array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// // for (let i = 0; i < array.length; i++) {
+// //     console.log(array[i]);
+// // }
+// array.forEach(a => {
+//     console.log(a);
+// });
+
+
+// let age = 20;
+// if (age >= 18) {
+//     console.log("You are eligible to vote.");
+// } else if (age < 0) {
+//     console.log("You are not eligible to vote.");
+// }
+// else {
+//     console.log("You are not eligible to vote.");
+// }
+
+
+// (method) Doucument.getElementById(id: string): HTMLElement | null
+// // let element = document.getElementById("heading");        
+
+
+// Doucument.getElementById("heading").innerHTML = "Hello World from JavaScript!";
+// get
+
+
+DocumentTimeline.getElementsByTagnmae('p')[2].innerHTML = '<h2>last Tag Updated by JavaScript</h2>';
+
+
+
+
+<></>
